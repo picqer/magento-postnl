c1e224f7ca04fdfca8698d87e9b5741c8ad408f5
Stephan Groen <stephan@picqer.com>
2018-03-30 14:35:57 +0200

    Correctly show and hide OrderGrid PostNL options

--- app/code/community/Picqer/PostNL/Model/Adminhtml/Observer/OrderGrid.php
+++ app/code/community/Picqer/PostNL/Model/Adminhtml/Observer/OrderGrid.php
@@ -2,30 +2,13 @@
 
 class Picqer_PostNL_Model_Adminhtml_Observer_OrderGrid extends TIG_PostNL_Model_Adminhtml_Observer_OrderGrid
 {
-
     public function modifyGrid(Varien_Event_Observer $observer)
     {
-        if($this->extensionActive()) {
+        $helper = Mage::helper('picqer_postnl');
+        if ($helper->isExtensionActive() && ! $helper->showPostNLOptions()) {
             return $observer;
         }
 
         return parent::modifyGrid($observer);
     }
-
-    /**
-     * Check whether this extension is active
-     * @return bool
-     */
-    private function extensionActive()
-    {
-        $active = Mage::getStoreConfig('picqer_shipping_options/postnl_settings/picqer_postnl_active');
-        if ($active != 1)
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-
 }
